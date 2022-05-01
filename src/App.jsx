8a1df9b5b5aa08885deb0a3d159eaeeef6763270
Luan Argolo <ipsluan@icloud.com>
2022-05-01 11:08:25 -0300

    :sparkles: feat: adicionar Button

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './components/Header';
 import Planets from './components/Planets';
+import Button from './components/Button';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Planets />
+      <Button title="Go to the next planet" />
     </div>
   );
 };
