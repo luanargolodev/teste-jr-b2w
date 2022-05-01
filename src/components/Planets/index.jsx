708e4490fa37dc78dad9624b300019da5cb142cc
Luan Argolo <ipsluan@icloud.com>
2022-05-01 11:31:32 -0300

    :sparkles: feat: adicionar API star wars

--- src/components/Planets/index.jsx
+++ src/components/Planets/index.jsx
@@ -1,25 +1,46 @@
+import React, { useState, useEffect } from 'react';
 import styles from './styles.module.css';
 
 const Planets = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    function getPlanet() {
+      const planet = Math.floor(Math.random() * 60) + 1;
+      fetch(`https://swapi.dev/api/planets/${planet}/`)
+        .then((res) => res.json())
+        .then((data) => setData(data))
+        .catch((err) => console.log(err));
+    }
+
+    getPlanet();
+  }, []);
+
   return (
     <main className={styles.main}>
-      <div className={styles.card}>
-        <h1>Mustafar</h1>
-        <div className={styles.content}>
-          <p className={styles.info}>
-            Population: <span className={styles.info_number}>5000</span>
-          </p>
-          <p className={styles.info}>
-            Population: <span className={styles.info_number}>5000</span>
-          </p>
-          <p className={styles.info}>
-            Population: <span className={styles.info_number}>5000</span>
-          </p>
-          <p className={styles.info}>
-            Population: <span className={styles.info_number}>5000</span>
-          </p>
+      {data && (
+        <div className={styles.card}>
+          <h1>{data.name}</h1>
+          <div className={styles.content}>
+            <p className={styles.info}>
+              Population:{' '}
+              <span className={styles.info_number}>{data.population}</span>
+            </p>
+            <p className={styles.info}>
+              Climate:{' '}
+              <span className={styles.info_number}>{data.climate}</span>
+            </p>
+            <p className={styles.info}>
+              Terrain:{' '}
+              <span className={styles.info_number}>{data.terrain}</span>
+            </p>
+            <p className={styles.info}>
+              Featured in the films:{' '}
+              <span className={styles.info_number}>{data.films.length}</span>
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </main>
   );
 };
